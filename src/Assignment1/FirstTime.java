2c84c6b33a777122ead74ea51697b025a991b8ad
smitaKothari <smita.s.kothari@gmail.com>
2022-02-22 15:39:06 -0600

    Added first program from Assignment1

---
+++ src/Assignment1/FirstTime.java
@@ -0,0 +1,15 @@
+package src.Assignment1;/*
+Program 1: src.Assignment1.FirstTime
+
+This one is short and sweet to help you get your first success.
+Name your class: src.Assignment1.FirstTime
+ */
+
+public class FirstTime {
+
+    public static void main(String args[]){
+        System.out.println("It never works the first time.");
+        System.out.println("It never works the second time, either.");
+        System.out.println("Be patient. You'll get it!");
+    }
+}
