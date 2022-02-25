ee62421d6eff31309e9ef529e9b39b55c49b13c5
smitaKothari <smita.s.kothari@gmail.com>
2022-02-25 15:30:26 -0600

    Added Poem program to assignment2

---
+++ src/Assignment2/Poem.java
@@ -0,0 +1,48 @@
+/*
+Write a Poem and print with different special characters.
+ */
+
+package src.Assignment2;
+
+import static java.lang.System.out;
+
+public class Poem {
+    public static void main (String args[]){
+        out.println("JABBERWOCKY \n");
+        out.println("\'Twas brillig, and the slithy toves");
+        out.println("\t Did gyre and gimble in the wabe;");
+        out.println("All mimsy were the borogoves,");
+        out.println("\t And the mome raths outgrabe.\n");
+        out.println("\"Beware the Jabberwock, my son!");
+        out.print("\t The jaws that bite, the claws that catch!");
+        out.println("Beware the jubjub bird, and shun");
+        out.println("The frumious Bandersnatch!\" \n");
+        out.println("He took his vorpal sword in hand:");
+        out.println("\t long time the manxome foe he sought -");
+        out.println("So rested he by the Tumtum tree,");
+        out.println("\t And stood awhile in thought. \n");
+        out.println("And as in uffish thought he stood,");
+        out.println("\t The Jabberwock, with eyes of flame,");
+        out.println("Came whiffling through the tulgey wood,");
+        out.println("\t And burbled as it came! \n");
+        out.println("One, two! One, two! And through and through");
+        out.println("\t The vorpal blade went snicker-snack!");
+        out.println("He left it dead, and with its head");
+        out.println("\t He went galumphing back. \n");
+        out.println("\"And hast thou slain the Jabberwock?");
+        out.println("\t Come to my arms, my beamish boy!");
+        out.println("O frabjous day! Callooh! Callay!\"");
+        out.println("\t He chortled in his joy.\n");
+        out.println("\'Twas brillig, and the slithy toves");
+        out.println("\t Did gyre and gimble in the wabe;");
+        out.println("All mimsy were the borogoves,");
+        out.println("\t And the mome raths outgrabe.\n");
+        out.println("\"It seems very pretty,\" she said when she had finished");
+        out.println("it, \"but it\'s RATHER hard to understand!\" (You see");
+        out.println("she didn't like to confess, ever to herself, that");
+        out.println("she couldn't make it out at all.) \"Somehow it seems");
+        out.println("to fill my head with ideas - only i don't exactly");
+        out.println("know what they are! However, SOMEBODY killed");
+        out.println("SOMETHING: that\'s clear, at any rate.\"");
+    }
+}
